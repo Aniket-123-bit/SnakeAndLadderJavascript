85e146f6fcec1f968314a3ea53ec0b483d4d9153
Aniket-123-bit <anisunny782@gmail.com>
2021-08-07 15:46:48 +0530

    [Aniket]ADDED code for Winning position

--- javascript/snakeAndLadderProbelm.js
+++ javascript/snakeAndLadderProbelm.js
@@ -1,8 +1,5 @@
-/* The Player then checks for a Option. They are No Play, Ladder or Snake. 
- * Use ((RANDOM)) to check for Options along with rolling a die
- * In Case of No Play the player stays in the same position
- * In Case of Ladder the player moves ahead by the number of position received in the die
- * In Case of Snake the player moves behind by the number of position received in the die
+/* Repeat till the Player reaches the winning position 100. 
+ * Note: In case the player position moves below 0, then the player restarts from 0
  *
  * @author: ANIKET RAIKWAR
  * DATE: 07/08/2021
@@ -14,24 +11,29 @@ const snake = 2;
 console.log("Welcome to SnakeAndLadder Probelm");
 console.log("******************************");
 console.log("Snake and Ladder game played with single player at start position 0");
-let initial_position = 0;
+//let initial_position = 0;
+//let final_position = 100;
+let playerPosition = 0;
+let winPosition = 100;
+for(playerPosition=0; playerPosition<winPosition; playerPosition++) {
 
 let roll_die = (Math.floor (Math.random() * 6 ) + 1);
 let check_options = (Math.floor( Math.random() * 10 ) ) % 3;
 
+if(playerPosition < 0) {
 switch(check_options){
 case noPlay:
-initial_position = 0;
+    playerPosition = 0;
 console.log("Its a NO PLAY! Player stays in the same position!");
 break;
 
 case ladder:
-initial_position += roll_die;
+    playerPosition += roll_die;
 console.log("Its a LADDER! Player moves ahead by " +roll_die+ " positions.");
 break;
 
 case snake:
-initial_position -= roll_die;
+    playerPosition -= roll_die;
 console.log("Its a SNAKE! Player moves behind by " +roll_die+ " positions.");
 break;
 
@@ -39,7 +41,10 @@ default:
 console.log("Error! Please check!");
 break;
 }
-if(initial_position < 0){
-initial_position = 0;
-console.log("Current position of the player in the game is: " +initial_position);
+
+}
+
+console.log("Player position is: "+playerPosition);
+
+    
 }
