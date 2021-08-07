5b8d7008fa6c4a28e6ab4aa3384a91aa560e630f
Aniket-123-bit <anisunny782@gmail.com>
2021-08-07 13:17:56 +0530

    [Aniket]ADDED for code Random Nuber Dice

--- javascript/snakeAndLadderProbelm.js
+++ javascript/snakeAndLadderProbelm.js
@@ -1,8 +1,9 @@
 /* Initialise the game
- *
+ * Use Random to get Dice Number between 1 to 6
  * @author: ANIKET RAIKWAR
  * DATE: 07/08/2021
  */
 console.log("Welcome to SnakeAndLadder Probelm");
 console.log("******************************");
 console.log("Snake and Ladder game played with single player at start position 0");
+console.log("Dice number is: " +(Math.floor (Math.random() * 6 ) + 1));
