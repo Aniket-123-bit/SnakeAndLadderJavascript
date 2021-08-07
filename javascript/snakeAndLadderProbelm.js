a8ffa825360b401b0da4d2c2ec3a6f84f7da21ee
Aniket-123-bit <anisunny782@gmail.com>
2021-08-07 13:10:42 +0530

    [Aniket]ADDED code for SnakeAndLadder

---
+++ javascript/snakeAndLadderProbelm.js
@@ -0,0 +1,8 @@
+/* Initialise the game
+ *
+ * @author: ANIKET RAIKWAR
+ * DATE: 07/08/2021
+ */
+console.log("Welcome to SnakeAndLadder Probelm");
+console.log("******************************");
+console.log("Snake and Ladder game played with single player at start position 0");
